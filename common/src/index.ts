f4e0899201469e465fd47da57217b53e9ebb7c66
mane936 <53373350+mane936@users.noreply.github.com>
2023-07-26 18:14:57 +0200

    Updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,5 +1,4 @@
 export * from "./events/base-listener";
 export * from "./events/base-publisher";
 export * from "./events/subjects";
-export * from "./events/ticket-created-event";
 export * from "./events/auth-verify-event";
